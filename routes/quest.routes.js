26757a4996aa51717a5cb9aec67d2affc354de4d
navitat <anlon636@gmail.com>
2025-08-06 15:53:54 +0200

    feat: GET quests

--- routes/quest.routes.js
+++ routes/quest.routes.js
@@ -5,7 +5,20 @@ const mongoose = require("mongoose");
 const Quest = require("../models/Quest.model");
 const { isAuthenticated } = require("../middleware/jwt.middleware");
 
-// POST /api/quests - create quest
+// GET /api/quests -- get quests
+router.get("/quests", isAuthenticated, (req, res, next) => {
+  const userId = req.payload._id;
+  Quest.find({ userId: userId })
+    .then((quests) => {
+      res.status(200).json(quests);
+    })
+    .catch((error) => {
+      console.log("Error retrieving quests");
+      next(error);
+    });
+});
+
+// POST /api/quests -- create quest
 router.post("/quests", isAuthenticated, (req, res, next) => {
   const { name, skillId } = req.body;
   const userId = req.payload._id;
