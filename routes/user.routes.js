aea21df821c15f2181730e72c416bb453625ada4
navitat <anlon636@gmail.com>
2025-08-06 14:55:38 +0200

    feat: get route for user

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -5,8 +5,26 @@ const mongoose = require("mongoose");
 const User = require("../models/User.model");
 const { isAuthenticated } = require("../middleware/jwt.middleware");
 
-// POST /api/users/disciplines -- create disciplines
-router.post("/users/disciplines", isAuthenticated, (req, res, next) => {
+// GET /api/user/
+router.get("/user", isAuthenticated, (req, res, next) => {
+  const userId = req.payload._id;
+
+  User.findById(userId)
+    .then((user) => {
+      if (!user) {
+        return res.status(404).json({ message: "User not found" });
+      }
+
+      res.status(200).json(user);
+    })
+    .catch((error) => {
+      console.log("Error retrieving user");
+      next(error);
+    });
+});
+
+// POST /api/user/disciplines -- create disciplines
+router.post("/user/disciplines", isAuthenticated, (req, res, next) => {
   const { name, type, skillId } = req.body;
   const userId = req.payload._id;
 
@@ -33,7 +51,7 @@ router.post("/users/disciplines", isAuthenticated, (req, res, next) => {
     });
 });
 
-// POST /api/users/skills -- create skills
+// POST /api/user/skills -- create skills
 router.post("/users/skills", isAuthenticated, (req, res, next) => {
   const userId = req.payload._id;
   const { name } = req.body;
